fd9dd805a1fb8439ed96b5dee55d07b6cbf2d628
Ecoleplus <743038-ecoleplus@users.noreply.gitlab.com>
2024-12-02 10:35:05 +0100

    card

--- config/ecoleplus-ui.php
+++ config/ecoleplus-ui.php
@@ -33,6 +33,12 @@ return [
         'input' => [
             'base' => 'border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm',
         ],
+        'card' => [
+            'base' => 'overflow-hidden',
+        ],
+        'card-section' => [
+            'base' => 'last:border-b-0',
+        ],
         'select' => [
             'base' => 'border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm',
         ],
